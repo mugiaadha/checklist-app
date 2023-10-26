602f74c8db862b084897a01f5c790ba32de56ea9
mugiaadha <mugia@investree.id>
2023-10-26 17:48:55 +0700

    first commit

---
+++ src/services/user.service.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import authHeader from '@/services/auth-header';
+
+const API_URL = '94.74.86.174:8080/api';
+
+class UserService {
+  getPublicContent() {
+    return axios.get(API_URL + 'all');
+  }
+
+  getUserBoard() {
+    return axios.get(API_URL + 'user', { headers: authHeader() });
+  }
+
+  getModeratorBoard() {
+    return axios.get(API_URL + 'mod', { headers: authHeader() });
+  }
+
+  getAdminBoard() {
+    return axios.get(API_URL + 'admin', { headers: authHeader() });
+  }
+}
+
+export default new UserService();
